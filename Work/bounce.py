a427752219a2330fad47c9151dd6ad84e92483a0
qliu2014 <quan.liu.engr@icloud.com>
2022-06-08 22:00:30 -0400

    Update bounce.py
    debugged and ran

--- Work/bounce.py
+++ Work/bounce.py
@@ -5,9 +5,9 @@
 height = 100.0
 count = 0
 
-for x in range(10)
+for x in range(10):
     height = height * 3 / 5
-    count++
-    print(count + " " + round(height, 2))
+    count += 1
+    print(str(count) + " " + str(round(height, 2)))
     
     
